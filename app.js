22b296667e9f6148d4aa5b9dd3837e07f4e558d2
Muhammad-Shariq1 <Shariqamin82@gmail.com>
2025-09-27 00:00:24 +0500

    image slider change

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-
 let NextDom = document.getElementById('Next');
 let PrevDom = document.getElementById('Prev');
 
 let CarouselDom = document.querySelector('.Carousel');
 let SliderDom = document.querySelector('.Carousel .List');
 let ThumbnailBorderDom = document.querySelector('.Carousel .Thumbnail');
-let ThumbnailItemsDom = ThumbnailBorderDom.querySelectorAll('.Items');
 let Timedom = document.querySelector('.Carousel .Time');
- 
-NextDom.onclick = function(){
+
+NextDom.onclick = function() {
     ShowSlider('Next');
-}
+};
 
+PrevDom.onclick = function() {
+    ShowSlider('Prev');
+};
 
-let TimeRunning =2000;
+let TimeRunning = 2000;
 let RunTimeOut;
 
-function ShowSlider (type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.Carousel .List .Items');
-    let  ThumbnailItemsDom = document.querySelectorAll('.Carousel .Thumbnail .Items');
+function ShowSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.Items');
+    let ThumbnailItemsDom = ThumbnailBorderDom.querySelectorAll('.Items');
 
-    if(type === 'Next'){
+    if (type === 'Next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         ThumbnailBorderDom.appendChild(ThumbnailItemsDom[0]);
-        CarouselDom.ClassList.add('Next');
+        CarouselDom.classList.add('Next');
+    } else if (type === 'Prev') {
+        let lastSliderItem = SliderItemsDom[SliderItemsDom.length - 1];
+        let lastThumbnailItem = ThumbnailItemsDom[ThumbnailItemsDom.length - 1];
+        SliderDom.prepend(lastSliderItem);
+        ThumbnailBorderDom.prepend(lastThumbnailItem);
+        CarouselDom.classList.add('Prev');
     }
 
-clearTimeout(RunTimeOut);
-RunTimeOut = setTimeOut (() => {
-    CarouselDom.ClassList.remove('next');
-}, TimeRunning );
-
+    clearTimeout(RunTimeOut);
+    RunTimeOut = setTimeout(() => {
+        CarouselDom.classList.remove('Next');
+        CarouselDom.classList.remove('Prev');
+    }, TimeRunning);
 }
